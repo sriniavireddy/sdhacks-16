9c44dd194fcceb8f89bc4f940e582ccc348aec8a
Joe Manley <joemanley@Joes-MacBook-Pro.local>
2016-10-02 00:19:45 -0700

    File Upload CSV, parse in Python

--- app/app.py
+++ app/app.py
@@ -3,6 +3,9 @@ from flask import Flask
 from flask import request
 import json
 import pandas as pd
+import jsonify
+import csv
+import io
 
 
 app = Flask(__name__)
@@ -11,6 +14,17 @@ app = Flask(__name__)
 def index():
 	print request.method, "hello"
 	return render_template('index.html')
+
+@app.route("/upload_csv", methods=['GET', 'POST'])
+def add_numbers():
+	uploaded_file = request.files['uploaded_file']
+
+	stream = io.StringIO(uploaded_file.stream.read().decode("UTF8"), newline=None)
+	uploaded_csv = csv.reader(stream)
+	for row in uploaded_csv:
+		print row
+
+	return "File Uploaded"
 
 if __name__ == '__main__':
 	app.run(debug=True)
