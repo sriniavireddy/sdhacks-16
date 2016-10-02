71e662bbe77f1535dabf1e4ba3ce4f158a6b3f1e
Joe Manley <joemanley@Joes-MacBook-Pro.local>
2016-10-01 22:27:52 -0700

    Implement File Upload

--- app/app.py
+++ app/app.py
@@ -7,8 +7,9 @@ import pandas as pd
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/", methods=['GET', 'POST'])
 def index():
+	print request.method, "hello"
 	return render_template('index.html')
 
 if __name__ == '__main__':
